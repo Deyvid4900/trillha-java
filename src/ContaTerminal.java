3677b6f7e56060505cd67276a4cca9eedfda3bb0
Deyvid Oliveira Silva <94657946+Deyvid4900@users.noreply.github.com>
2024-04-29 16:10:14 -0300

    :tada: Commit inicial

---
+++ src/ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o numero da conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite a agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo da conta:");
+        float saldoConta = scanner.nextFloat();
+
+        System.out.println("A conta do(a) "+ nomeCliente +" foi criada com "+saldoConta+" reais");
+
+    }
+}
